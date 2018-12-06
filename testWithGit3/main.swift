cbf4d19e5749fac4c00afb0a7755d64554653f57
kanatev <aleksey.kanatev@mail.ru>
2018-12-06 12:18:01 +0500

    Initial Commit

---
+++ testWithGit3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testWithGit3
+//
+//  Created by AlexMacPro on 06/12/2018.
+//  Copyright © 2018 AlexMacPro. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
